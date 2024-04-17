4858f1cbcdbff35f26e1beb742aaec7e74185d80
Blue-Kite <bluelemon112@gmail.com>
2024-04-17 22:45:48 +0900

    [chore] eslint 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import React from "react";
 import "./App.css";
 
 function App() {
+  const [test, setTest] = React.useState<string>('hello')
   return (
-    <p>깃허브 연동 테스트</p>
+    <>
+      <p>깃허브 연동 테스트</p>
+      <p>{test}</p>      
+    </>
+  
+    
   );
 }
 
