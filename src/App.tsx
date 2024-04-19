fb573faac342bbcce2076a4c2dcb632499ec3a72
Blue-Kite <bluelemon112@gmail.com>
2024-04-19 15:19:02 +0900

    [chore] eslint 설정 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
-  const [test, setTest] = React.useState<string>('hello')
-  return (
-    <>
-      <p>깃허브 연동 테스트</p>
-      <p>{test}</p>      
-    </>
-  
-    
-  );
+	const [test] = React.useState<string>('hello');
+	return (
+		<>
+			<p>깃허브 연동 테스트</p>
+			<p>{test}</p>
+			<p>husky 재설정</p>
+		</>
+	);
 }
 
 export default App;
