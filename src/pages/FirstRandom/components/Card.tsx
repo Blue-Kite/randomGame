a0d78f53b73e41c2b603dbd653ce535bd05c4136
Blue-Kite <bluelemon112@gmail.com>
2024-06-21 12:44:10 +0900

    [feat] 카드 뒤집기

--- src/pages/FirstRandom/components/Card.tsx
+++ src/pages/FirstRandom/components/Card.tsx
@@ -1,10 +1,31 @@
 import { Box } from '@chakra-ui/react';
+import React from 'react';
 
 type CardProps = {
 	isLoser: boolean;
 };
-const Card = ({ isLoser }: CardProps) => (
-	<Box width="100%" height="200px" background={isLoser ? 'red' : 'green'} />
-);
+
+const Card = ({ isLoser }: CardProps) => {
+	const [isFront, setIsFront] = React.useState(true);
+
+	const handleFlip = () => {
+		setIsFront(!isFront);
+	};
+
+	let background;
+	if (isFront) background = 'green';
+	else if (isLoser) background = 'red';
+	else background = 'blue';
+
+	return (
+		<Box
+			onClick={handleFlip}
+			width="100%"
+			height="200px"
+			background={background}
+			borderRadius="10px"
+		/>
+	);
+};
 
 export default Card;
